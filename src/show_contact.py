6fb9162e753149a0367bbc9e0bfc865466157c13
Olha <142577158+Helga735@users.noreply.github.com>
2023-12-16 19:52:23 +0200

    Added comments

--- src/show_contact.py
+++ src/show_contact.py
@@ -7,12 +7,17 @@ class Contact:
         self.birthday = birthday
 
     def days_until_birthday(self):
+        # Отримаємо сьогоднішню дату
         today = datetime.now().date()
+        # Обчислити наступний день народження
         next_birthday = datetime(today.year, self.birthday.month, self.birthday.day).date()
 
+        # Перевірка, чи не минув день народження в цьому році
         if today > next_birthday:
+            # Якщо минув, обчислити наступний день народження на наступний рік
             days_lefs = datetime(today.year + 1, self.birthday.month, self.birthday.day).date()
 
+        # Обчислити дні, що залишилися до наступного дня народження
         days_lefs = (next_birthday - today).days
         return days_lefs
     
@@ -22,12 +27,17 @@ class PersonalAssistant:
         self.contacts = contacts
     
     def show_upcoming_birthdays(self, days):
+        # Отримаємо поточну дату та час
         today = datetime.now()
+        # Список для зберігання майбутніх днів народження
         upcoming_birthdays = []
 
         for contact in self.contacts:
+            # Підрахувати дні, що залишилися до дня народження контакту
             days_lefs = contact.days_until_birthday()
+            # Перевірка, чи день народження знаходиться в межах вказаної кількості днів
             if 0 < days_lefs <= days:
+                # Додайте ім'я контакту та дні, що залишилися, до списку upcoming_birthdays
                 upcoming_birthdays.append((contact.name, days_lefs))
         
         return upcoming_birthdays
